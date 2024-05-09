e18ffe89b556941366191741438785f0c4b7138f
nichapa-nop <165654040+nichapa-nop@users.noreply.github.com>
2024-05-09 19:54:13 +0700

    add create role

--- src/store/db/accountdb/accountdb.module.ts
+++ src/store/db/accountdb/accountdb.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import configService from 'src/config/config.service';
 import { AccountEntity } from 'src/model/account/entities/account.entity';
+import { RoleEntity } from 'src/model/role/entities/role.entity';
 
 @Module({
     imports: [
         TypeOrmModule.forRoot({
             type: 'postgres',
             ...configService.getConfig().database,
-            entities: [AccountEntity],
+            entities: [AccountEntity, RoleEntity],
             synchronize: true,
         }),
     ],
